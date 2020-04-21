8ca21b0f978c404ef8198c92d264fb3f6b1273ec
hideaki-hayashida <kuon.bell@gmail.com>
2020-04-21 22:49:33 +0900

    ケアレスミス修正、外部キーに該当する部分をreferences型に修正

--- db/migrate/20200421125819_create_groups.rb
+++ db/migrate/20200421125819_create_groups.rb
@@ -1,10 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string id, :null false
-      t.string name, :null false
-      t.integer user_id, :foreign_key: true
-      t.tnteger post_id, :foreign_key: true
+      t.string :id, :null false
+      t.string :name, :null false
+      t.references :user_id, :foreign_key: true
+      t.references :post_id, :foreign_key: true
       t.timestamps
 
       has_many :posts through: :user_id
--- db/migrate/20200421130324_create_posts.rb
+++ db/migrate/20200421130324_create_posts.rb
@@ -1,12 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.integer id, null: false
-      t.text post, null: false
-      t.integer user_id, foreign_key: true
-      t.integer group_id, foreign_key: true
+      t.integer :id, null: false
+      t.text :post, null: false
+      t.references :user_id, foreign_key: true
+      t.references :group_id, foreign_key: true
       t.timestamps
-      
+
       belong_to :user
       belong_to :group
 
