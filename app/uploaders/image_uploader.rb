983081c54517e17f185ee907d80a3b32f69bacf0
hideaki-hayashida <kuon.bell@gmail.com>
2020-05-14 21:13:02 +0900

    S3関連

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,6 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  storage :fog
 
   # Create different versions of your uploaded files:
   # version :thumb do
